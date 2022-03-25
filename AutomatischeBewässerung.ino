9a17e6ed237d62c1a9b638466a88046cfb571cdb
FritzWilli <kirschbluetenbau@gmail.com>
2022-03-25 22:12:30 +0100

    Added VS Code Files

---
+++ AutomatischeBewässerung.ino
@@ -0,0 +1,15 @@
+﻿/*
+ Name:		AutomatischeBewässerung.ino
+ Created:	3/25/2022 10:08:02 PM
+ Author:	Vitas Ruhland
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
